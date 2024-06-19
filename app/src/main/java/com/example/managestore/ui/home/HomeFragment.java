667c7761f5e50feba4bcb1029440db04ee1ce628
Quoc Tin <tin.9a1.dt@gmail.com>
2024-06-19 09:08:07 +0700

    doing home page

--- app/src/main/java/com/example/managestore/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/managestore/ui/home/HomeFragment.java
@@ -4,28 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.managestore.dao.ProductDAO;
 import com.example.managestore.databinding.FragmentHomeBinding;
+import com.example.managestore.models.Product;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    RecyclerView horizontalList;
+    ListView verticalList;
+    private ProductDAO productDAO;
+    HorizontalListAdapter horizontalListAdapter;
+    VerticalListAdapter verticalListAdapter;
+    List<Product> productList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        horizontalList = binding.horizontalList;
+        verticalList = binding.verticalList;
+
+        productDAO = new ProductDAO(getActivity());
+        productDAO.open();
+
+        productList = productDAO.getProducts();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(root.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        horizontalListAdapter = new HorizontalListAdapter(root.getContext(), productList);
+        horizontalList.setAdapter(horizontalListAdapter);
+        horizontalList.setLayoutManager(layoutManager);
+
+        verticalListAdapter = new VerticalListAdapter(root.getContext(), productList);
+        verticalList.setAdapter(verticalListAdapter);
+
         return root;
     }
 
---
+++ app/src/main/java/com/example/managestore/ui/home/VerticalListAdapter.java
@@ -0,0 +1,67 @@
+package com.example.managestore.ui.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.managestore.R;
+import com.example.managestore.models.Product;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class VerticalListAdapter extends BaseAdapter {
+    Context context;
+    List<Product> listProduct;
+    LayoutInflater inflater;
+
+    public VerticalListAdapter(Context ctx, List<Product> productList) {
+        this.context = ctx;
+        this.listProduct = productList;
+        inflater = LayoutInflater.from(ctx);
+    }
+
+    @Override
+    public int getCount() {
+        return this.listProduct.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = inflater.inflate(R.layout.product_horizontal, null);
+
+        TextView name = convertView.findViewById(R.id.horizontalItemName);
+        TextView category = convertView.findViewById(R.id.horizontalItemCategory);
+        TextView price = convertView.findViewById(R.id.horizontalItemPrice);
+        ImageView img = convertView.findViewById(R.id.horizontalItemImg);
+
+        name.setText(listProduct.get(position).getProductName());
+        category.setText(listProduct.get(position).getCategoryName());
+
+        BigDecimal bigDecimal = new BigDecimal(listProduct.get(position).getProductPrice());
+        DecimalFormat formatter = new DecimalFormat("#,###,###");
+
+        price.setText(formatter.format(bigDecimal) + " VND");
+
+        Glide.with(context).load(listProduct.get(position).getProductLink()).into(img);
+
+        return convertView;
+    }
+}
+
